512473e411488bf4ab8b859910dc0ea5a3fe5a32
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-08-31 17:21:45 +0300

    commit tree 3e9ecc67e067dd11d774b29eb11a166ed7a5236e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < Sinatra::Base
+ set :default_content_type, 'application/json'
+  get '/games' do
+    
 
-  get '/' do
-    { message: "Hello world" }.to_json
+   games=Game.all.order(:title).limit(10)
+   games.to_json
+   
+  end
+  get "/games/:id" do 
+    games =Game.find(params[:id])
+     games.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
